2b6a122bcc410c766344a3ea03e7faddc1d252c4
techs-sus <92276908+techs-sus@users.noreply.github.com>
2025-11-15 12:02:22 -0500

    Add bit32 translation tests for Luau

--- test/translation/transformation/luau/luauSpecificTransformations.ts
+++ test/translation/transformation/luau/luauSpecificTransformations.ts
@@ -7,3 +7,10 @@ while (false) {
 do {
     continue;
 } while (false);
+
+const a = 34193128 >>> 23;
+const b = 34193128 << 23;
+const c = 34193128 ^ 23;
+const d = 34193128 & 23;
+const e = ~d;
+const f = 34193128 | 23;
